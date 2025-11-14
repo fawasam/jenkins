9bc4e5590192df9b7f22b1303ecb401d05d34f2d
fawasam <fawas@webcastle.in>
2025-11-15 03:01:10 +0530

    Update response message in index.js to reflect version 2 of the Node.js Docker container.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const server = http.createServer((req, res) => {
   }
 
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from Node.js Docker container!\n");
+  res.end("Hello from Node.js Docker container v2!\n");
 });
 
 server.listen(PORT, HOST, () => {
